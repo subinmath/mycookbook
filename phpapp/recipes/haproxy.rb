5429a68c4bb2fa033b7286e213f2a0fdb503b411
subinmath <subin.mathew@crichq.com>
2016-08-11 12:37:05 +0530

    Create haproxy.rb

---
+++ phpapp/recipes/haproxy.rb
@@ -0,0 +1,27 @@
+
+package 'haproxy' do
+  action :install
+end
+
+if platform?('debian','ubuntu')
+  template '/etc/default/haproxy' do
+    source 'haproxy-default.erb'
+    owner 'root'
+    group 'root'
+    mode 0644
+  end
+end
+
+include_recipe 'haproxy::service'
+
+service 'haproxy' do
+  action [:enable, :start]
+end
+
+template '/etc/haproxy/haproxy.cfg' do
+  source 'haproxy.cfg.erb'
+  owner 'root'
+  group 'root'
+  mode 0644
+  notifies :restart, "service[haproxy]"
+end
