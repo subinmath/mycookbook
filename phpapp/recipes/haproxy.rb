462dd7e1fcea4308f2949bf8199132be5a47a9dc
subinmath <subin.mathew@crichq.com>
2016-08-12 15:14:26 +0530

    Update haproxy.rb

--- phpapp/recipes/haproxy.rb
+++ phpapp/recipes/haproxy.rb
@@ -2,4 +2,12 @@
 package 'haproxy' do
   action :install
 end
+if platform?('debian','ubuntu')
+   template '/etc/default/haproxy' do
+    source 'haproxy-default.erb'
+    owner 'root'
+   group 'root'
+   mode 0644
+ end
+ end
 
